2130f6917ed06e568831068f2da393ee5f4f05fd
MALET Jean-Luc <jeanluc.malet@gmail.com>
2009-06-02 21:28:02 +0200

    napper speed improvement and reliability improvement, shared lib detection modification to use autoapp/autolib

--- tests/C/test_shared_static.sh
+++ tests/C/test_shared_static.sh
@@ -7,48 +7,6 @@
 #************************************************************************/
 
 mytest() {
-        clean() {
-                rm -rf *.o *.so *.a *.exe *.dll testlibconfiserie
-        }
-
-        create_objects() {
-                ${CC} -c -o libconfiserie.o ${CPPFLAGS} ${CFLAGS} \
-                      ${confiserie}/C/libconfiserie/libconfiserie.c 
-                ${CC} -c -o libconfiserie2.o ${CPPFLAGS} ${CFLAGS} \
-                      ${confiserie}/C/libconfiserie/libconfiserie2.c 
-        }
-
-        test_dll() {
-                echo "----------------------------"
-                echo "testing if dll are supported"
-                ${CC} ${CC_LDFLAGS} -shared \
-                -o libconfiserie.dll -Wl,--out-implib=libconfiserie.dll.a\
-                -Wl,--export-all-symbols -Wl,--enable-auto-import \
-                -Wl,--no-whole-archive  libconfiserie.o libconfiserie2.o&&
-                ${CC} ${CC_LDFLAGS} -L. -lconfiserie -o testlibconfiserie \
-                ${confiserie}/C/testlibconfiserie.c libconfiserie.dll.a&&
-                SHAREDLIBEXT=".dll"
-        }
-
-        test_shared() {
-                echo "----------------------------"
-                echo "testing if elf .so are supported"
-                ${LD} ${LDFLAGS} -shared -Bdynamic -soname libconfiserie.so \
-                -o libconfiserie.so  libconfiserie.o libconfiserie2.o &&
-                ${CC} ${CC_LDFLAGS} -L ./ -lconfiserie -o testlibconfiserie  \
-                ${confiserie}/C/testlibconfiserie.c &&
-                SHAREDLIBEXT=".so"
-        }
-
-        test_static() {
-                echo "----------------------------"
-                echo "testing if ar archives are supported"
-                ${AR} -cr ${ARFLAGS} libconfiserie.a  libconfiserie.o libconfiserie2.o &&
-                ${RANLIB} libconfiserie.a &&
-                ${CC} ${CC_LDFLAGS} -o testlibconfiserie ${confiserie}/C/testlibconfiserie.c \
-                ./libconfiserie.a &&
-                STATICLIBEXT=".a"
-        }
 
 
         CC=${CC:=gcc}
@@ -60,20 +18,38 @@ mytest() {
 
         echo ${TEST_SEPARATOR} >&2
         echo test_shared_static : testing library extention >&2
+        cp -R ${confiserie}/C/libconfiserie/ .tmp &&
+        cd .tmp                                   &&
+        {
 
-        create_objects 1>/dev/null &&
-        if [ -z "$ENABLE_STATIC" ]; then
-                echo "WARNING : ENABLE_STATIC env not set, static libs will not be build"
-                echo "          unless shared libs aren't available"
-        else
-                test_static 
-        fi &&
-        if [ -z "$DISABLE_DYNAMIC" ]; then
-                echo "DISABLE_DYNAMIC env not set => testing for shared lib format"
-                test_shared || test_dll || test_static
-        fi &&
+                if [ -z "$ENABLE_STATIC" ]; then
+                        echo "WARNING : ENABLE_STATIC env not set, static libs will not be build"
+                        echo "          unless shared libs aren't available"
+                else
+                        echo "----------------------------"
+                        echo "testing if ar archives are supported"
+                        export STATICLIBEXT=".a";
+                        make || unset STATICLIBEXT;
+                fi &&
+                if [ -z "$DISABLE_DYNAMIC" ]; then
+                        echo "DISABLE_DYNAMIC env not set => testing for shared lib format"
+                        {
+                                echo "----------------------------"
+                                echo "testing if elf .so are supported"
+                                export SHAREDLIBEXT=".so";
+                                make
+                        } ||
+                        {
+                                echo "----------------------------"
+                                echo "testing if dll are supported"
+                                export SHAREDLIBEXT=".dll";
+                                make
+                        } || unset SHAREDLIBEXT
+                fi
+        }
 
-        clean
+        cd --
+        rm -r .tmp
 
         conf_cache STATICLIBEXT
         conf_cache SHAREDLIBEXT
