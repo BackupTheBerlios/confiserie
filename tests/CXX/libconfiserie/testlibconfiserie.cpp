2022951893388752a65028aa6fbe772e5c770bf7
MALET Jean-Luc <jeanluc.malet@gmail.com>
2009-06-04 14:23:23 +0200

    added autolib extension detection for C++

--- tests/CXX/libconfiserie/testlibconfiserie.cpp
+++ tests/CXX/libconfiserie/testlibconfiserie.cpp
@@ -1,18 +1,18 @@
-#!/bin/sh
 #/***********************************************************************
 #* copyright (c) 2004-2006 MALET Jean-luc aka cityhunter
 #* This library is free software; you can redistribute it and/or modify
 #* it under the terms of the LGPL v2.1 or latter at your option
 #* http://www.gnu.org/copyleft/lesser.html
 #************************************************************************/
-confiserie=${confiserie:=..}
-[ -z "${COMMON_TEST_DEFINITION_SOURCED}" ] && source ${confiserie}/common_test_definition.sh
+#include "libconfiserie.h"
 
-mytest() {
 
-        [ -z "${CC}" ] && runtest ${confiserie}/C/test_cc.sh
-        export LIB_DIR=${confiserie}/C/libconfiserie/
-        runtest ${confiserie}/script/Compiler_test_shared_static.sh
+
+int main(int argc, char** argv)
+{
+        confiserie sweet;
+        sweet.tata();
+        int err;
+        err = do_nothing2();
+        if (err) return err;
 }
-
-mytest
